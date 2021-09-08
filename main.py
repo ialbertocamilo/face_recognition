4d2584d5cbc0db0ac196355b56b315d9b1f7ac9d
Alberto Camilo Rodriguez Vizcarra <41647415+ialbertocamilo@users.noreply.github.com>
2021-09-08 14:56:30 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import cv2
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
